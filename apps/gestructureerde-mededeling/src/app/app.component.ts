c7f7432fe770551115d30c5aa810e62f59d69ec8
Maarten Tibau <maarten@webtrix.be>
2020-09-19 09:22:24 +0200

    chore(upgrade): upgrade to v10.2.1

--- apps/gestructureerde-mededeling/src/app/app.component.ts
+++ apps/gestructureerde-mededeling/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { ScreenService } from './core/services/screen.service';
 import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { ScreenService } from './core/services/screen.service';
 
 @Component({
   selector: 'ogm-app-root',
