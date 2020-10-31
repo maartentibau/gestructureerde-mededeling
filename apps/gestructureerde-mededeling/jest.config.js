a672504af23ee5965977d198c650f4aeeb432ef6
Maarten Tibau <maarten@webtrix.be>
2020-10-31 12:22:36 +0100

    chore(testing): fix issue with astTransformers setting after upgrade to Jest v26

--- apps/gestructureerde-mededeling/jest.config.js
+++ apps/gestructureerde-mededeling/jest.config.js
@@ -12,10 +12,7 @@ module.exports = {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
+      astTransformers: { before: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer'] }
     },
   },
 };
