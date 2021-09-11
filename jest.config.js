11103a6e12a16274ccaeae31e400de5eb8f00b20
Maarten Tibau <maarten@webtrix.be>
2021-09-11 13:42:50 +0200

    chore(upgrade): upgrade Nx to v12.9

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  projects: ['<rootDir>/apps/gestructureerde-mededeling'],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: [...getJestProjects(), '<rootDir>/apps/gestructureerde-mededeling'] };
